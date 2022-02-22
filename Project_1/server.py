e1ed3a88e62c71e7c343a3bda51014369a76ed91
masjohncook <mas.john.cook@gmail.com>
2022-02-22 18:04:06 +0800

    feat - add server.py file to run at server machine

---
+++ Project_1/server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import socket
+
+HOST = '<Server_IP_Address>'
+PORT = 1234
+BUFFER_SIZE = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(HOST, PORT)
+s.listen(1)
+
+conn, addr = s.accept()
+print("connection address: " + str(addr))
+
+while True:
+    data = conn.recv(BUFFER_SIZE)
+    if not  data:
+        print('Client close connection')
+        break
+    print('recv: ' + data.decode())
+    out_data = 'echo: ' + data.decode()
+    conn.send(out_data.encode())
+conn.close()
